26c11e946a665cb9312c962d9c10b81529b56b2d
Francisco <fcisco.dev@gmail.com>
2022-01-06 23:51:29 +0700

    change data type to text

--- database/migrations/1_migration_up.sql
+++ database/migrations/1_migration_up.sql
@@ -8,7 +8,7 @@ CREATE TABLE "articles" (
   "id" uuid PRIMARY KEY,
   "author_id" uuid,
   "title" varchar,
-  "body" varchar,
+  "body" text,
   "created_at" timestamp
 );
 ALTER TABLE "articles" ADD FOREIGN KEY ("author_id") REFERENCES "authors" ("id");
